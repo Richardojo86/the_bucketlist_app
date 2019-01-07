f636ee69650f7a17a1eaac654664d9e2a981ddcc
Susan Esho <susan.esho@andela.com>
2019-01-07 22:24:56 +0100

    sm code

--- app/controllers/bucketlists_controller.rb
+++ app/controllers/bucketlists_controller.rb
@@ -1,16 +1,34 @@
 class BucketlistsController < ApplicationController
-  # def index
-  #   @bucketlists = Bucketlist.all
-  #   respond_to do |format|
-  #     format.html { render: index }
-  #     format.json { render json: @bucketlists, each_serializer: BucketlistSerializer }
-  #   end
-  # end
 
-  def new
-    @bucketlist = Bucketlist.new(params[:name]) 
+  def index
+    @bucketlists = Bucketlist.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @bucketlists, each_serializer: BucketlistSerializer }
+    end
   end
 
+  def new
+    @bucketlist = Bucketlist.new(params[:name])
+    respond_to do |format|
+      format.html { render :template => "bucketlists/_form"}
+      format.js { }
+    end
+  end
+
+  def create
+    @bucketlist  = Bucketlist.create(bucket_params)
+    flash[:notice] = "successfully created"
+    redirect_to bucketlists_path
+    # respond_to do |format|
+    #   format.html { redirect_to bucketlists_path }
+    #   format.js { }
+    # end
+  end
+
+  # def show
+  #   @bucketlist = Bucketlist.find(params[:id])
+  # end
 
   # def create
   #   @bucketlist = Bucketlist.new(name)
@@ -22,11 +40,23 @@ class BucketlistsController < ApplicationController
   #   end
   # end
 
-  def create
-    @bucketlist = Bucketlist.create(params[:name])
-    respond_to do |format|
-      format.html { redirect_to bucketlist_path }
-      format.js
-    end
+  # def create
+  #   @bucketlist = Bucketlist.create(params[:name])
+  #   respond_to do |format|
+  #     format.html { redirect_to bucketlist_path }
+  #     format.js
+  #   end
+  # end
+  #
+
+
+  # def all_bucketlists
+  #   @bucketlist = Bucketlist.all
+  # end
+
+  private
+
+  def bucket_params
+    params.require(:bucketlist).permit(:name)
   end
 end
--- test/controllers/bucketlists_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BucketlistsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
